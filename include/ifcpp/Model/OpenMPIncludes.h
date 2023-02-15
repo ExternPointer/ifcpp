39e1069c2fdfe1cbd1fc3e80f5da2f487cc2fe8d
ExternPointer <extern.pointer@gmail.com>
2023-02-15 18:15:53 +0200

    wip

--- include/ifcpp/Model/OpenMPIncludes.h
+++ include/ifcpp/Model/OpenMPIncludes.h
@@ -22,6 +22,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OU
 		#define ENABLE_OPENMP
 	#endif
 #endif
+
+#ifdef ENABLE_OPENMP
 #include <omp.h>
 
 class Mutex 
@@ -70,3 +72,4 @@ public:
 protected:
 	Mutex&	m_mutex; 
 };
+#endif
